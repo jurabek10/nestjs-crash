119f75cf3146fc1006f018c88df110cdaccd1a1b
jurabek10 <samiev.jurabek@bk.ru>
2025-08-29 01:15:51 +0900

    feat: integrate with env

--- apps/nestjs-batch/src/nestjs-batch.module.ts
+++ apps/nestjs-batch/src/nestjs-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NestjsBatchController } from './nestjs-batch.controller';
 import { NestjsBatchService } from './nestjs-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NestjsBatchController],
   providers: [NestjsBatchService],
 })
--- apps/nestjs-batch/src/nestjs-batch.service.ts
+++ apps/nestjs-batch/src/nestjs-batch.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class NestjsBatchService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Batch';
   }
 }
